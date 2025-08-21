53296bf5b8fad4c024aef0e2582060418e4b7762
Sego <isaacenzo126@gmail.com>
2025-08-21 12:08:33 -0300

    Alterações JavaScript

--- trabalho (REAL)/js/pagCursos.js
+++ trabalho (REAL)/js/pagCursos.js
@@ -1,5 +1,21 @@
-function buttonSobre(){
-    const Sobre = document.querySelector('.pags-sobre')
+const body = document.body
+const Sobre = document.querySelector('.pags-sobre');
+const overlay = document.getElementById('overlay');
+const buttonAbrir = document.getElementById('butaoAbrido')
+const buttonFechar = document.getElementById('butaoFechar')
+
+
+/*function buttonAbrir(){
+    const Sobre = document.querySelector('.pags-sobre');
     Sobre.hidden = false;
     Sobre.removeAttribute('hidden');
+    body.style.backdropFilter = "blur(5px)";
+    body.style.backgroundColor = "rgba(255,255,255,0.5)";
 }
+
+function buttonFechar(){
+    const Sobre = document.querySelector('.pags-sobre')
+    Sobre.hidden = true;
+    Sobre.setAttribute('hidden', '');
+
+}*/
