3b746ffd91a969ecc99fbd5bf002d48888691633
Sego <isaacenzo126@gmail.com>
2025-08-25 17:50:11 -0300

    Atualizando pag Todos os Cursos

--- trabalho (REAL)/js/todosCursos.js
+++ trabalho (REAL)/js/todosCursos.js
@@ -127,3 +127,166 @@ function buttonFechar8(){
     document.body.style.overflow = 'auto';
 }
 /*Excel Básico FIM*/ 
+
+/*Comércio*/
+/*Administração Financeira no Coméricio*/
+  const SobreCo = document.querySelector('#curso-Adf');
+  function buttonAbrir9(){
+    SobreCo.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar9(){
+    SobreCo.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Administração Financeira no Coméricio Fim*/
+  
+  /*Como Vender na Internet e Redes Socias*/
+  const SobreCoa = document.querySelector('#curso-Ven');
+  function buttonAbrir10(){
+    SobreCoa.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar10(){
+    SobreCoa.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Como Vender na Internet e Redes Socias Fim*/
+   
+  /*Empreendedorismo Digital*/
+  const SobreCob = document.querySelector('#curso-Emp');
+  function buttonAbrir11(){
+    SobreCob.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar11(){
+    SobreCob.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Empreendedorismo Digital Fim*/
+  
+  /*Estratégias de Negociação para o Vendedor*/
+  const SobreCoc = document.querySelector('#curso-Est');
+  function buttonAbrir12(){
+    SobreCoc.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar12(){
+    SobreCoc.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Estratégias de Negociação para o Vendedor Fim*/
+
+  /*Ferramentas de Marketing Digital*/
+  const SobreCod = document.querySelector('#curso-Mar');
+  function buttonAbrir13(){
+    SobreCod.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar13(){
+    SobreCod.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Ferramentas de Marketing Digital Fim*/
+  
+  /*Planejamento de Marketing para Gestores*/
+  const SobreCoe = document.querySelector('#curso-Ges');
+  function buttonAbrir14(){
+    SobreCoe.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar14(){
+    SobreCoe.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Planejamento de Marketing para Gestores Fim*/
+  
+  /*Planejamento Financeiro para o Comércio*/
+  const SobreCof = document.querySelector('#curso-Com');
+  function buttonAbrir15(){
+    SobreCof.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar15(){
+    SobreCof.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Planejamento Financeiro para o Comércio Fim*/
+  
+  /*Técnicas Avançandas de Vendas e Atendimento*/
+  const SobreCog = document.querySelector('#curso-Ate');
+  function buttonAbrir16(){
+    SobreCog.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function buttonFechar16(){
+    SobreCog.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+  /*Técnicas Avançandas de Vendas e Atendimento Fim*/
+  
+  /*Saúde*/
+  const SobreSa = document.querySelector('#curso-Lim')
+  function buttonAbrir17(){
+    SobreSa.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+}
+
+function buttonFechar17(){
+    SobreSa.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+}
+
+/* Massagem Relaxante */
+const SobreSaa = document.querySelector('#curso-ml');
+function buttonAbrir18(){
+    SobreSaa.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+}
+
+function buttonFechar18(){
+    SobreSaa.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+}
+
+/* Primeiros Socorros */
+const SobreSab = document.querySelector('#curso-ps');
+function buttonAbrir19(){
+    SobreSab.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
+}
+
+function buttonFechar19(){
+    SobreSab.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+}
